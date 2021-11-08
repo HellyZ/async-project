aa116e17425555c8d777cc77a3e6dd4bf4367cf5
Alena Zhmurko <alenazhmurko@gmail.com>
2021-11-08 19:07:40 +0200

    fix db.json structure

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-let dbData;
-const getData = () => {
-  fetch("db.json")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      dbData = data
-    })
-    .catch((error) => {
-      console.log("getData error", error);
-    });
-};
+const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
+const LOCAL_JSON_PATH = "db.json";
+
+const getData = (path) => {
+  try {
+    const response = fetch(path).then((response) => response.json());
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const sendData = (url, data) => {
+  console.log(`Sending via fetch to ${url} data ${data}`);
   fetch(url, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
+      Accept: "application/json, text/plain, */*",
       "Content-type": "application/json; charset=UTF-8",
     },
   })
-    .then((response) => response.json())
-    .then((json) => console.log(json))
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonData) => jsonData)
     .catch((error) => {
       console.log("sendData error", error);
     });
-};
+}
 
-getData();
-sendData("https://jsonplaceholder.typicode.com/posts", dbData);
+getData(LOCAL_JSON_PATH).then((res) => {
+  sendData(POSTS_URL, res);
+});
+
+getData(POSTS_URL).then((res) => console.log(res));
