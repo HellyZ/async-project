ffa839539ab3444d332588eb45efca241e2f8dfa
Alena Zhmurko <alenazhmurko@gmail.com>
2021-11-08 17:54:42 +0200

    fix sendData func

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-const dataTag = document.querySelector(".data");
-const tbl = document.createElement("table");
-const tblBody = document.createElement("tbody");
-dataTag.append(tbl);
-dataTag.append(tblBody);
-let dataResult;
-
-
+let dbData;
 const getData = () => {
-
-  const myData = (result) => {
-    dataResult = result
-  };
-  fetch("https://jsonplaceholder.typicode.com/posts")
+  fetch("db.json")
     .then((response) => response.json())
     .then((data) => {
-      newData = data;
-      return data;
-    })
-    .then((data) => {myData(data); return data;}).then((data) => {
-      let dataContainer = data;
-      Array(dataContainer).forEach((el) => {
-        el.forEach((key, value) => {
-          let newRow = document.createElement("tr");
-          let newCell = document.createElement("td");
-          let newContent = JSON.stringify(key, value);
-          newCell.append(newContent);
-          newRow.append(newCell);
-          tblBody.append(newRow);
-          tbl.append(tblBody);
-        });
-      });
       console.log(data);
-      return data;
+      dbData = data
     })
     .catch((error) => {
       console.log("getData error", error);
@@ -46,18 +19,12 @@ const sendData = (url, data) => {
       "Content-type": "application/json; charset=UTF-8",
     },
   })
-    .then((response) => {
-      response.json();
-      console.log("sendData");
-    })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
     .catch((error) => {
       console.log("sendData error", error);
     });
 };
 
-document.addEventListener("DOMContentLoaded", (e) => {
-  console.log("Hello");
-  getData();
-  console.log(dataResult);
-  sendData("https://jsonplaceholder.typicode.com/posts", dataResult);
-});
+getData();
+sendData("https://jsonplaceholder.typicode.com/posts", dbData);
